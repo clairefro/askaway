d92433b46dcb10a5967635c2eead8f4aa87a16c6
Claire Froelich <claire.froelich@gmail.com>
2021-02-17 23:02:56 -0800

    Change logo font

--- packages/client/src/components/blocks/Navbar.tsx
+++ packages/client/src/components/blocks/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar: FC = () => {
     <nav className="py-2 px-6 w-full flex justify-between">
       <div className="flex items-center">
         <Link to="/">
-          <span className="text-xl sm:text-2xl text-white">askaway</span>
+          <span className="x-logo-text text-4xl text-white">askaway</span>
         </Link>
       </div>
     </nav>
