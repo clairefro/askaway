04e6772b4f3dca960cb1a2fd5135f081665af32b
Claire Froelich <claire.froelich@gmail.com>
2021-02-17 18:51:54 -0800

    Connect server to mongodb

---
+++ packages/api/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const port = process.env.PORT || 4444
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+  })
+);
+
+// connect to db and start server
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("MongoDB Connected...");
+	})
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`Example app listening at http://localhost:${port}`);
+		});
+	})
+	.catch((e) => console.log(e));
